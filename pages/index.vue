93aec279373bcf2ac3ccafb6fbdde6b6d3aa09b5
hoonki <chaeefe@gmail.com>
2019-03-24 21:27:25 +0900

    add tabs

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-  <v-carousel hide-delimiters>
+  <v-carousel>
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
       :src="item.src"
     ></v-carousel-item>
   </v-carousel>
+  <v-tabs
+  centered
+  color="white"
+  icons-and-text
+  >
+    <v-tabs-slider color="white"></v-tabs-slider>
 
+    <v-tab href="#tab-1">
+      Recents
+      <v-icon>phone</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-2">
+      Favorites
+      <v-icon>favorite</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-3">
+      Nearby
+      <v-icon>account_box</v-icon>
+    </v-tab>
+
+    <v-tab-item
+      v-for="i in 3"
+      :key="i"
+      :value="'tab-' + i"
+    >
+      <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+      </v-card>
+    </v-tab-item>
+  </v-tabs>
   <v-container>
     <v-layout row wrap>
       <v-flex xs12 sm 12 md12>
@@ -24,13 +55,14 @@
                 max="5"
               >
                 <v-item v-for="n in length" :key="n">
-
                   <v-icon
                   slot-scope="{active,toggle}"
                   :input-value="active"
                   icon
-                  @click="toggle">fiber_manual_record</v-icon>
-
+                  color="grey"
+                  @click="toggle">
+                    fiber_manual_record
+                  </v-icon>
                 </v-item>
               </v-item-group>
             </v-flex>
