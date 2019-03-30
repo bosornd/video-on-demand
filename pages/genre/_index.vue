7c66c9dc7be17c8317f85b645d135d786ca2e0c3
soobeen <b2nyb2st@naver.com>
2019-03-30 20:08:49 +0900

    movie page
    hover, clear(no shadow), title position to left

--- pages/genre/_index.vue
+++ pages/genre/_index.vue
@@ -25,12 +25,32 @@
           v-for="(card) in latest"
           :key="card.title"
           xs6 sm4 md3
+
         >
-            <v-card nuxt-link :to="'/movie/' + card._id">
+        <v-hover>
+            <v-card
+            slot-scope="{ hover }"
+
+            nuxt-link :to="'/movie/' + card._id"
+            boolean flat false>
               <v-img
                 :src="'http://image.tmdb.org/t/p/w185/' + card.poster"
                 height="370px"
               >
+              <v-transition class="fade-transition">
+                <div
+                  v-if="hover"
+                  class="d-flex black darken-2 v-card--reveal white--text"
+                  style="height: 100%"
+                >
+                  <div class="pa-4">
+                    <p><v-icon color="yellow">star</v-icon> {{card.vote_average}}</p>
+                    <h2>{{card.title}}</h2>
+                    <p><h3>{{card.description.slice(0,150)}}...</h3></p>
+                  </div>
+                </div>
+              </v-transition>
+
                 <v-container
                   fluid
                   fill-height
@@ -41,13 +61,17 @@
 
                 </v-container>
               </v-img>
-              <v-card-title primary-title>
+              <v-card-title primary-title left>
+
+
+
                <div>
                  {{card.title}}
                  <div> {{ card_text }} </div>
                </div>
               </v-card-title>
             </v-card>
+            </v-hover>
           </v-flex>
         </v-layout>
       </v-flex>
@@ -83,3 +107,15 @@ export default {
   }
 }
 </script>
+
+<style scoped="">
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .8;
+  position: absolute;
+  width: 100%;
+}
+
+</style>
