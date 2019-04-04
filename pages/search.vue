df6000f155f52338f7a88b03b7fa71a7e836cac2
parkjinyoung <pjpp8588@naver.com>
2019-04-04 20:11:04 +0900

    update Search page (table to card)

--- pages/search.vue
+++ pages/search.vue
@@ -5,18 +5,40 @@
         <v-text-field v-model="title" prepend-icon="search" label="Title or Description" @keyup.enter="search"></v-text-field>
       </v-flex>
       <v-flex v-if="searched.length" xs12>searched movies</v-flex>
-      <v-flex v-if="searched.length" xs12>
-        <v-data-table v-model="selected" :headers="headers" :items="searched"
-          item-key="title" select-all :pagination.sync="table_attribute" class="elevation-1">
-          <template slot="items" slot-scope="props">
-            <td><v-checkbox v-model="props.selected" primary hide-details></v-checkbox></td>
-            <td class="text-xs-center"><nuxt-link :to="'/movie/' + props.item._id">{{props.item.title}}</nuxt-link></td>
-            <td class="text-xs-center"><v-btn v-for="(genre,i) in props.item.genres" :key="i"><nuxt-link :to="'/genre/' + genre.name">{{genre.name}}</nuxt-link></v-btn></td>
-            <td class="text-xs-center">{{props.item.released_date}}</td>
-            <td class="text-xs-center">{{props.item.vote_average}}</td>
-          </template>
-        </v-data-table>
-      </v-flex>
+      <v-container v-if="searched.length" fluid grid-list-md xs12>
+        <v-layout row wrap>
+          <v-flex v-for="card in searched" :key="card.title" xs12 sm4 md2>
+            <v-hover>
+              <v-card
+                slot-scope="{hover}"
+                class="mx-auto"
+                max-width="400"
+                height="360"
+                :to="'/movie/' + card._id"
+              >
+              <v-img :src="card.poster" height=360px>
+                <transition class="fade-transition">
+                  <div
+                    v-if="hover"
+                    class="d-flex black darken-2 v-card--reveal white--text"
+                    style="height: 100%"
+                  >
+                    <div class="pa-4">
+                      <p><v-icon color="yellow">star</v-icon> {{card.vote_average}}</p>
+                      <h2>{{card.title}}</h2>
+                      <p><h3>{{card.description.slice(0,100)}}...</h3></p>
+                    </div>
+                  </div>
+                </transition>
+              </v-img>
+            </v-card>
+          </v-hover>
+          <div>
+            <span><h3>{{card.title}}</h3></span>
+          </div>
+        </v-flex>
+        </v-layout>
+      </v-container>
     </v-layout>
   </v-container>
 </template>
@@ -25,22 +47,11 @@
 export default {
   data () {
     return {
-      selected: [],
-      headers: [
-        { text: 'title', align: 'center', value: 'title' },
-        { text: 'genres', align: 'center', value: 'genres' },
-        { text: 'released', align: 'center', value: 'released_date' },
-        { text: 'rate', align: 'center', value: 'vote_average' }
-      ],
-      table_attribute: {
-        descending: true,
-        rowsPerPage: 10,
-        sortBy: 'released_date'
-      },
       title: '',
       searched: []
     }
   },
+
   methods: {
     async search () {
       console.log(this.title)
